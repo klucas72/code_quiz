c5540a5f3891148e66752e3e7433ee48e8736670
klucas72 <klucas72@hotmail.com>
2021-03-10 17:14:44 -0500

    4th push

--- assets/script.js
+++ assets/script.js
@@ -1,190 +1,70 @@
- // a variable for start time
-let secondsLeft = 76;
-
-//the element that displays the time
-let timer = document.getElementById("timer");
-
-//div for high scores
-let scoresDiv = document.getElementById("scores-div");
-
-let buttonsDiv = document.getElementById("buttons")
-
-//button for high scores
-let viewScoresBtn = document.getElementById("view-scores")
-
-//start button div
-let startButton = document.getElementById("start-button");
-startButton.addEventListener("click", setTime);
-
-
-// variable for the questions title
-var questionDiv = document.getElementById("question-div");
-
-// div to hold the results
-let results = document.getElementById("results");
-
-// div for the choices
+var scores = document.getElementById("view-scores");
+var timer = document.getElementById("timer");
+var scoresDiv = document.getElementById("scores-div");
 var choices = document.getElementById("choices");
+var results = document.getElementById("results");
+var start = document.getElementById("start-button");
 
+var secondsLeft = 60;
+var setTime = secondsLeft
 
-// an array to store high scores
-let emptyArray = [];
+const questions = [
+    [
+        {
+          question: "Who invented JavaScript?",
+          answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "Which one of these is a JavaScript package manager?",
+          answers: {
+            a: "Node.js",
+            b: "TypeScript",
+            c: "npm"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "Which tool can you use to ensure code quality?",
+          answers: {
+            a: "Angular",
+            b: "jQuery",
+            c: "RequireJS",
+            d: "ESLint"
+          },
+          correctAnswer: "d"
+        },
+    ],
+];
 
-// the array of high scores from local storage
-let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
-
-// keeping track of which question we're on
-var questionCount = 0;
-
-//keeping score
-let score = 0
-
-//Timer starts when the user clicks startButton (see above).
-function setTime() {
-  displayQuestions();
-  let timerInterval = setInterval(function() {
+start.addEventListener("click", function(setTime) {
+  console.log("i was clicked")
+  var timerInterval = setInterval(function() {
     secondsLeft--;
-    timer.textContent = "";
-    timer.textContent = "Time: " + secondsLeft;
-    if (secondsLeft <= 0 || questionCount === questions.length) {
+    timer.textContent = secondsLeft + " seconds left";
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
       clearInterval(timerInterval);
-      captureUserScore();
-    } 
-  }, 1000);
-}
-
-//function to load the questions on the page
-function displayQuestions() {
-  removeEls(startButton);
-
-  if (questionCount < questions.length) {
-    questionDiv.innerHTML = questions[questionCount].title;
-    choices.textContent = "";
-
-    for (let i = 0; i < questions[questionCount].multiChoice.length; i++) {
-      let el = document.createElement("button");
-      el.innerText = questions[questionCount].multiChoice[i];
-      el.setAttribute("data-id", i);
-      el.addEventListener("click", function (event) {
-        event.stopPropagation();
-
-        if (el.innerText === questions[questionCount].answer) {
-          score += secondsLeft;
-        } else {
-          score -= 10;
-          secondsLeft = secondsLeft - 15;
-        }
-        
-        questionDiv.innerHTML = "";
-
-        if (questionCount === questions.length) {
-          return;
-        } else {
-          questionCount++;
-          displayQuestions();
-        }
-      });
-      choices.append(el);
+      // Calls function to create and send message
+      sendMessage();
     }
-  }
+    // function displayQuestions(questions) {
+    //   console.log("I am showing")
+    //   var output = [];
+    //   var answers;
+    //   for (var i=0; i<questions.length; i++){
+    //     answers = [];
+        
+    //   }
+    // };
+  }, 1000);
+});
+
+function sendMessage() {
+  timer.textContent = "time's up!";
 }
-
-
-function captureUserScore() {
-  timer.remove();
-  choices.textContent = "";
-
-  let initialsInput = document.createElement("input");
-  let postScoreBtn = document.createElement("input");
-
-  results.innerHTML = `You scored ${score} points! Enter initials: `;
-  initialsInput.setAttribute("type", "text");
-  postScoreBtn.setAttribute("type", "button");
-  postScoreBtn.setAttribute("value", "Post My Score!");
-  postScoreBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    let scoresArray = defineScoresArray(storedArray, emptyArray);
-
-    let initials = initialsInput.value;
-    let userAndScore = {
-      initials: initials,
-      score: score,
-    };
-
-    scoresArray.push(userAndScore);
-    saveScores(scoresArray);
-    displayAllScores();
-    clearScoresBtn();
-    goBackBtn();
-    viewScoresBtn.remove();
-  });
-  results.append(initialsInput);
-  results.append(postScoreBtn);
-}
-
-const saveScores = (array) => {
-  window.localStorage.setItem("highScores", JSON.stringify(array));
-}
-
-const defineScoresArray = (arr1, arr2) => {
-  if(arr1 !== null) {
-    return arr1
-  } else {
-    return arr2
-  }
-}
-
-const removeEls = (...els) => {
-  for (let el of els) el.remove();
-}
-
-function displayAllScores() {
-  removeEls(timer, startButton, results);
-  let scoresArray = defineScoresArray(storedArray, emptyArray);
-
-  scoresArray.forEach(obj => {
-    let initials = obj.initials;
-    let storedScore = obj.score;
-    let resultsP = document.createElement("p");
-    resultsP.innerText = `${initials}: ${storedScore}`;
-    scoresDiv.append(resultsP);
-  });
-}
-
-function viewScores() {
-  viewScoresBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    removeEls(timer, startButton);
-    displayAllScores();
-    removeEls(viewScoresBtn);
-    clearScoresBtn();
-    goBackBtn();
-  });
-}
-
-function clearScoresBtn() {    
-  let clearBtn = document.createElement("input");
-  clearBtn.setAttribute("type", "button");
-  clearBtn.setAttribute("value", "Clear Scores");
-  clearBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    removeEls(scoresDiv);
-    window.localStorage.removeItem("highScores");
-  })
-  scoresDiv.append(clearBtn)
-}
-
-function goBackBtn() {
-  let backBtn = document.createElement("input");
-  backBtn.setAttribute("type", "button");
-  backBtn.setAttribute("value", "Go Back");
-  backBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    window.location.reload();
-  })
-  buttonsDiv.append(backBtn)
-}
-
-
-viewScores();
-
+setTime
