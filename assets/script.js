ef227efbbb80475b756db31c09ed1852c061b31a
klucas72 <klucas72@hotmail.com>
2021-03-10 21:50:52 -0500

    5th push

--- assets/script.js
+++ assets/script.js
@@ -4,45 +4,49 @@ var scoresDiv = document.getElementById("scores-div");
 var choices = document.getElementById("choices");
 var results = document.getElementById("results");
 var start = document.getElementById("start-button");
+var questionsBox = document.getElementById("question-div")
 
 var secondsLeft = 60;
-var setTime = secondsLeft
+var currentQuestion = 0;
+var setTime = secondsLeft;
 
-const questions = [
-    [
+const questions = [ 
         {
           question: "Who invented JavaScript?",
-          answers: {
-            a: "Douglas Crockford",
-            b: "Sheryl Sandberg",
-            c: "Brendan Eich"
-          },
+          answers: [
+            "a: Douglas Crockford",
+            "b: Sheryl Sandberg",
+            "c: Brendan Eich"
+          ],
           correctAnswer: "c"
         },
         {
           question: "Which one of these is a JavaScript package manager?",
-          answers: {
-            a: "Node.js",
-            b: "TypeScript",
-            c: "npm"
-          },
+          answers: [
+            "a: Node.js",
+            "b: TypeScript",
+            "c: npm"
+          ],
           correctAnswer: "c"
         },
         {
           question: "Which tool can you use to ensure code quality?",
-          answers: {
-            a: "Angular",
-            b: "jQuery",
-            c: "RequireJS",
-            d: "ESLint"
-          },
+          answers: [
+            "a: Angular",
+            "b: jQuery",
+            "c: RequireJS",
+            "d: ESLint"
+          ],
           correctAnswer: "d"
-        },
-    ],
+        },    
 ];
 
 start.addEventListener("click", function(setTime) {
-  console.log("i was clicked")
+  startTimer();
+  getQuestion();
+});
+
+function startTimer(){
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timer.textContent = secondsLeft + " seconds left";
@@ -52,19 +56,44 @@ start.addEventListener("click", function(setTime) {
       // Calls function to create and send message
       sendMessage();
     }
-    // function displayQuestions(questions) {
-    //   console.log("I am showing")
-    //   var output = [];
-    //   var answers;
-    //   for (var i=0; i<questions.length; i++){
-    //     answers = [];
-        
-    //   }
-    // };
   }, 1000);
-});
+}
+//Obtaining questions from the questions inventory
+function getQuestion(){
+  removeEls(start);
+
+  for (var i = 0; i < questions[currentQuestion].answers.length; i++){
+    console.log(questions[currentQuestion].answers[i])
+    var button = document.createElement("button");
+    button.innerText = questions[currentQuestion].answers[i];
+    button.setAttribute("data-id", i);
+    button.addEventListener("click", function(event){
+      event.stopPropagation()
+
+      if (button.innerText === questions[currentQuestion].answer){
+        score += secondsLeft;
+      } else {
+        score -= 10;
+        secondsLeft = secondsLeft - 10;
+      }
+      questionsBox.innerHTML = "";
+      if (currentQuestion === questions.length){
+        return;
+      } else {
+        currentQuestion++;
+        getQuestion();
+      }
+    }
+  )};
+}
+ //wrap each answer in a button using inner-html
+// choices.innerHTML = "<button>" + questions[currentQuestion].answers[i] + "</button>";
 
 function sendMessage() {
   timer.textContent = "time's up!";
 }
 setTime
+
+const removeEls = (...els) => {
+    for (let el of els) el.remove();
+  }
