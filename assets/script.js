2d3915917f4ce682f71474f871e8e64aab20a74f
klucas72 <klucas72@hotmail.com>
2021-03-11 16:04:31 -0500

    6th push

--- assets/script.js
+++ assets/script.js
@@ -11,34 +11,34 @@ var currentQuestion = 0;
 var setTime = secondsLeft;
 
 const questions = [ 
-        {
-          question: "Who invented JavaScript?",
-          answers: [
-            "a: Douglas Crockford",
-            "b: Sheryl Sandberg",
-            "c: Brendan Eich"
-          ],
-          correctAnswer: "c"
-        },
-        {
-          question: "Which one of these is a JavaScript package manager?",
-          answers: [
-            "a: Node.js",
-            "b: TypeScript",
-            "c: npm"
-          ],
-          correctAnswer: "c"
-        },
-        {
-          question: "Which tool can you use to ensure code quality?",
-          answers: [
-            "a: Angular",
-            "b: jQuery",
-            "c: RequireJS",
-            "d: ESLint"
-          ],
-          correctAnswer: "d"
-        },    
+  {
+    question: "Who invented JavaScript?",
+    answers: [
+      "a: Douglas Crockford",
+      "b: Sheryl Sandberg",
+      "c: Brendan Eich"
+    ],
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: [
+      "a: Node.js",
+      "b: TypeScript",
+      "c: npm"
+    ],
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: [
+      "a: Angular",
+      "b: jQuery",
+      "c: RequireJS",
+      "d: ESLint"
+    ],
+    correctAnswer: "d"
+  },    
 ];
 
 start.addEventListener("click", function(setTime) {
@@ -61,39 +61,19 @@ function startTimer(){
 //Obtaining questions from the questions inventory
 function getQuestion(){
   removeEls(start);
-
+  
+  questionsBox.innerHTML = "<p>" + questions[currentQuestion].question + "</p>";
   for (var i = 0; i < questions[currentQuestion].answers.length; i++){
     console.log(questions[currentQuestion].answers[i])
-    var button = document.createElement("button");
-    button.innerText = questions[currentQuestion].answers[i];
-    button.setAttribute("data-id", i);
-    button.addEventListener("click", function(event){
-      event.stopPropagation()
-
-      if (button.innerText === questions[currentQuestion].answer){
-        score += secondsLeft;
-      } else {
-        score -= 10;
-        secondsLeft = secondsLeft - 10;
-      }
-      questionsBox.innerHTML = "";
-      if (currentQuestion === questions.length){
-        return;
-      } else {
-        currentQuestion++;
-        getQuestion();
-      }
-    }
-  )};
-}
- //wrap each answer in a button using inner-html
-// choices.innerHTML = "<button>" + questions[currentQuestion].answers[i] + "</button>";
+    choices.innerHTML += "<button>" + questions[currentQuestion].answers[i] + "</button>";
+  };
+};
 
 function sendMessage() {
   timer.textContent = "time's up!";
-}
-setTime
+  };
+setTime;
 
 const removeEls = (...els) => {
     for (let el of els) el.remove();
-  }
+  }; 
