ecd0d56c410ee0222dd9a719e11bf45323d66be4
klucas72 <klucas72@hotmail.com>
2021-03-13 10:04:35 -0500

    9th push

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//creating variables
 var scores = document.getElementById("view-scores");
 var timer = document.getElementById("timer");
 var scoresDiv = document.getElementById("scores-div");
@@ -11,6 +12,7 @@ var currentQuestion = 0;
 var setTime = secondsLeft;
 var score = 0
 
+//create questions
 const questions = [ 
   {
     question: "Who invented JavaScript?",
@@ -42,11 +44,13 @@ const questions = [
   },    
 ];
 
+//start quiz
 start.addEventListener("click", function(setTime) {
   startTimer();
   getQuestion();
 });
 
+//start timer
 function startTimer(){
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -63,8 +67,8 @@ function startTimer(){
 function getQuestion(){
   removeEls(start);
   resetAnswers();
-  questionsBox.innerHTML = "<p>" + questions[currentQuestion].question + "</p>";
-  for (var i = 0; i < questions[currentQuestion].answers.length; i++){
+  for (var i = 0; i < questions.length; i++){
+    questionsBox.innerHTML = "<p>" + questions[currentQuestion].question + "</p>";
     // console.log(questions[currentQuestion].answers[i])
     var button = document.createElement("button")
     button.dataset.index = i;
@@ -73,34 +77,53 @@ function getQuestion(){
     choices.append(button);
   };
 };
-
+//send message when time is up
 function sendMessage() {
   timer.textContent = "time's up!";
-  };
+};
 setTime;
 
+//stopping quiz
+function quizDone(){
+    console.log("before entering if" + questions[currentQuestion].question);
+    if (questions[currentQuestion].question < questions.length - 1) {
+        questions[currentQuestion].textContent = "quiz done!";
+        console.log("after entering if" + questions[currentQuestion].question);
+        return;
+      }
+  };
+
+  //check answers for questions listed
 function checkAnswer(e) {
-   console.log(e.target.dataset.index);
-   var selectedAnswerIdx = e.target.dataset.index
-   console.log(typeof selectedAnswerIdx);
-   if (questions[currentQuestion].correctAnswer == selectedAnswerIdx){
-     console.log("correct")
-     score += secondsLeft;
-   } else {
+  console.log(e.target.dataset.index);
+  var selectedAnswerIdx = e.target.dataset.index
+  console.log(typeof selectedAnswerIdx);
+  if (questions[currentQuestion].correctAnswer == selectedAnswerIdx){
+    console.log("correct")
+    score += secondsLeft;
+  } else {
     score -= 10;
     secondsLeft = secondsLeft - 10;
     console.log("incorrect")
-   }
-   currentQuestion++;
-   getQuestion();
-   
-}
+  }
+  currentQuestion++;
+  getQuestion();  
+};
+
+// reset the answers for each question
 function resetAnswers(){
   while (choices.firstChild) {
     choices.removeChild(choices.firstChild);
   };
 };
 
+//remove start button when quiz 
 const removeEls = (...els) => {
     for (let el of els) el.remove();
   }; 
+
+  //show scores
+  function captureScore() {
+    timer.remove();
+
+  }
